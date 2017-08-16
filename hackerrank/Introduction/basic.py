735f26dc74c891c864a09e4e3efbabaa327d3d38
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-08-16 23:55:26 +0300

    Added basic_dt.py

--- hackerrank/Introduction/basic.py
+++ hackerrank/Introduction/basic.py
@@ -1,34 +1,35 @@
 basic.py
 
 def main():
-#     n = int(input())
-#     if n % 2 == 1:
-#         print('Weird')
-#     elif n % 2 == 0 and (2 <= n <= 5):
-#         print('Not Weird')
-#     elif n % 2 == 0 and (6 <= n <= 20):  
-#         print('Weird')
-#     elif n % 2 == 0 and n > 20:
-#         print('Not Weird')
+    n = int(input())
+    if n % 2 == 1:
+        print('Weird')
+    elif n % 2 == 0 and (2 <= n <= 5):
+        print('Not Weird')
+    elif n % 2 == 0 and (6 <= n <= 20):  
+        print('Weird')
+    elif n % 2 == 0 and n > 20:
+        print('Not Weird')
     
-# if __name__ == '__main__':
-#     main()
-#    
+if __name__ == '__main__':
+    main()
+   
   
-    # elif year % 100 == 0:
-    #     return True
+    elif year % 100 == 0:
+        return True
 
 
-# def is_leap(year):
-#     if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
-#         return True
-#     else:
-#         return False  
+def is_leap(year):
+    if (year % 4 == 0 and year % 100 != 0) or (year % 400 == 0):
+        return True
+    else:
+        return False  
 
-# year = int(input()) 
-# print(is_leap(year))       
-#         
+year = int(input()) 
+print(is_leap(year))       
+        
 
+# Print Function
 # from __future__ import print_function
 # import sys, os, time
 
@@ -38,7 +39,6 @@ def main():
 #     print(x, sep=' ', end='')  
 #     time.sleep(1)
 
-# Print Function
 
 print(*range(1, int(input()) + 1), sep="")  
 
---
+++ hackerrank/basic_dt.py
@@ -0,0 +1,5 @@
+basic_dt.py
+
+
+print([int(i) for i in input('Type n space-separated integers: ').split()])
+print(hash(tuple([1, 2, 3])))
