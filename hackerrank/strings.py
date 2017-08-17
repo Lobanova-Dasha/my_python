224571a49541252cf79d697297b7fed659f76434
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-08-18 01:43:15 +0300

    strings.py was updated

--- hackerrank/strings.py
+++ hackerrank/strings.py
@@ -6,9 +6,9 @@ def swap_case(s):
     return s.swapcase()
 
 s = input()
-# print(swap_case(s))
+# # print(swap_case(s))
 
-#String Split and Join#
+# #String Split and Join#
 
 def split_and_join(line):
 
@@ -28,6 +28,7 @@ def print_full_name(fname, lname):
 
 
 #Mutations#
+
 def mutate_string(string, posit_char):
 	split_posit_char = posit_char.split()
 	position = int(split_posit_char[0])
@@ -35,17 +36,38 @@ def mutate_string(string, posit_char):
 	string = string[:position] + character + string[position+1:]
 	print(string)
     
-
-mutate_string(input(), input())
+# mutate_string(input(), input())
 
 # I prefer this way
 # def mutate_string(string, position, character):
 # 	string = string[:position] + character + string[position+1:]
 # 	print(string)
     
-
 #mutate_string(input(), int(input()), input())
 
+# Capitalize! #
+def capitalize(string):
+	split_str = string.split(" ")
+	ar = [i.capitalize() for i in split_str]
+	join_line = " ".join(ar)
+	print(join_line)
+	
+# capitalize(input())
+
+# print(" ".join(word.capitalize() for word in input().split(' ')))
+
+
+
+# String Formatting #
+
+n = int(input())
+width = len(bin(n)) - 2
+for x in range(1, (n + 1)):
+	print(('%d'%x).rjust(width), 
+		   ('%o'%x).rjust(width),
+		   ('%X'%x).rjust(width),
+		    (str(bin(x))[2:]).rjust(width))
+		   
 
 
 
