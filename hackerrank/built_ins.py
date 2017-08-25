85913db1d874c59ca575a2ff7e15c72fddac5851
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-08-25 23:36:17 +0300

    functionals.py was updated

--- hackerrank/built_ins.py
+++ hackerrank/built_ins.py
@@ -1,10 +1,56 @@
 # built-ins.py
 
-# Input()
+# Zipped! 
+n = list(map(int, input().split()))
 
+sheet = []
+for i in range(n[1]):
+    
+    scores = list(map(float, input().split()))
+    sheet.append(scores) 
+
+new_sheet = list(zip(*sheet))
+
+avr = list(map(sum, new_sheet))
+for x in avr:
+     print(float(x/n[1]))
+
+
+# Input()
 x, k = map(int, input().split())
 print(eval(input()) == k)
 
 
 # Python Evaluation
 eval(input())
+
+
+# ginortS
+line = input()
+
+alpha = list(filter((lambda x: x.isalpha()), line))
+
+lower = list(filter((lambda x: x.islower()), alpha))
+lower.sort()
+
+upper = list(filter((lambda x: x.isupper()), alpha))
+upper.sort()
+
+digits = list(filter((lambda x: x.isdigit()), line))
+digits = list(map(int, digits))
+
+odd = list(filter((lambda x: x%2==1), digits))
+odd.sort()
+
+even = list(filter((lambda x: x%2==0), digits))
+even.sort()
+
+new_digit = list(map(str, odd + even))
+
+my_seq = lower + upper + new_digit
+
+from functools import reduce 
+
+print(reduce((lambda x, y: x + y), my_seq))
+
+
--- hackerrank/functionals.py
+++ hackerrank/functionals.py
@@ -2,7 +2,7 @@
 # functionals.py
 
 # Map and Lambda Function
-cube = lambda x: x*x*x 
+cube = lambda x: x**3
 
 def fibonacci(n):
     fib = []
@@ -14,3 +14,33 @@ def fibonacci(n):
   
 
 print(list(map(cube, fibonacci(int(input()))))) 
+
+
+# Validating Email Addresses With a Filter
+import re
+
+def fun(s):
+    valid = re.match(r'[a-zA-Z0-9_-]+@[a-zA-Z0-9]+\.[a-zA-Z]{1,3}$',s)
+    return valid
+
+# import re
+# lst = []
+# for i in range(int(raw_input())):
+#     lst.append(raw_input())
+# print sorted(list(filter(lambda x: re.search(r'^[\w\d-]+@[A-Za-z0-9]+\.\w?\w?\w$',x),lst)))    
+
+
+
+# def filter_mail(emails):
+#     return list(filter(fun, emails))
+
+# if __name__ == '__main__':
+#     n = int(input())
+#     emails = []
+#     for i in range(n):
+#         emails.append(input())
+
+# filtered_emails = filter_mail(emails)
+# filtered_emails.sort()
+# print(filtered_emails)    
+
