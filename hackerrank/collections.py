557f7850b0da0e2aff2084adc7331a074a2dd9f3
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-08-31 02:42:16 +0300

    Updated collections.py

--- hackerrank/collections.py
+++ hackerrank/collections.py
@@ -63,3 +63,56 @@ for i in range(num_of_commands):
         dq.appendleft(int(choice[1]))
 
 print(*list(dq)) 
+
+
+
+# Word Order
+# You are given n words. Some words may repeat. 
+#For each word, output its number of occurrences. 
+# The output order should correspond with the input order 
+# of appearance of the word. 
+# See the sample input/output for clarification.
+from collections import Counter, OrderedDict
+
+class OrderedCounter(Counter, OrderedDict):
+    pass
+
+order_counter  = OrderedCounter(input() for _ in range(int(input())))
+
+print(len(order_counter))
+print(*order_counter.values())
+
+# This way failed :(
+from collections import Counter
+
+my_counter = Counter([input() for _ in range(int(input()))])
+
+res = [v for k, v in my_counter.items()]
+print(len(res))
+print(*(sorted(res, reverse=True)))
+
+
+# Most Common
+# You are given a string S. 
+# The string contains only lowercase English alphabet characters.
+# Your task is to find the top 
+# three most common characters in the string S.
+
+# This is my solution, but sometimes it fails several tests
+from collections import Counter, OrderedDict
+
+my_counter = Counter(sorted(input()))
+
+order_counter = OrderedDict([i for i in my_counter.most_common(3)])
+
+for k, v in order_counter.items():
+    print(k, v)    
+
+
+# This isn't my solution, but it always works
+from collections import Counter, OrderedDict
+
+class OrderedCounter(Counter, OrderedDict):
+    pass
+
+[print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
