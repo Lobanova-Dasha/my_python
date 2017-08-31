a06ce61e29d6508d75c3ff26f3f47c0a1cdac5c5
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-08-31 16:11:54 +0300

    Updated collections.py

--- hackerrank/collections.py
+++ hackerrank/collections.py
@@ -1,5 +1,8 @@
 #collections.py
 
+
+# collections.Counter()
+
 from collections import Counter
 
 numShoes = int(input())
@@ -15,6 +18,37 @@ for i in range(numCust):
         shoes[size] -= 1
 
 print(income)
+
+
+# DefaultDict Tutorial
+from collections import defaultdict
+
+my_dict = defaultdict(list)
+n, m = list(map(int, input().split()))
+
+for i in range(n):
+    my_dict[input()].append(i + 1)
+
+for i in range(m):
+    print(' '.join(map(str, my_dict[input()])) or -1)
+
+
+# Collections.namedtuple()
+# Task. Calculate the average marks of the students.
+
+# №1 using namedtuple
+from collections import namedtuple
+
+stu = int(input())
+marks = namedtuple('student', input())
+
+print(sum(float(marks(*input().split()).MARKS) for _ in range(stu))/stu)
+
+# №2 not using namedtuple
+stu =  int(input())
+marks = input().split().index("MARKS")
+
+print (sum([int(input().split()[marks]) for _ in range(stu)]) / stu)
 
 
 # Collections.OrderedDict()
@@ -43,29 +77,6 @@ for item_name, net_price in bill_order.items():
     print(item_name, net_price)
 
 
-# Collections.deque() 
-# Task. Perform append, pop, popleft and appendleft methods 
-# on an empty deque d.
-from collections import deque
-
-dq = deque()
-num_of_commands = int(input())
-
-for i in range(num_of_commands):
-    choice = input().split()
-    if choice[0] == "pop":
-        dq.pop()
-    elif choice[0] == "append":
-        dq.append(int(choice[1]))
-    elif choice[0]=="popleft" :
-        dq.popleft() 
-    elif choice[0] == "appendleft":
-        dq.appendleft(int(choice[1]))
-
-print(*list(dq)) 
-
-
-
 # Word Order
 # You are given n words. Some words may repeat. 
 #For each word, output its number of occurrences. 
@@ -92,6 +103,62 @@ print(len(res))
 print(*(sorted(res, reverse=True)))
 
 
+# Collections.deque() 
+# Task. Perform append, pop, popleft and appendleft methods 
+# on an empty deque d.
+from collections import deque
+
+dq = deque()
+num_of_commands = int(input())
+
+for i in range(num_of_commands):
+    choice = input().split()
+    if choice[0] == "pop":
+        dq.pop()
+    elif choice[0] == "append":
+        dq.append(int(choice[1]))
+    elif choice[0]=="popleft" :
+        dq.popleft() 
+    elif choice[0] == "appendleft":
+        dq.appendleft(int(choice[1]))
+
+print(*list(dq)) 
+
+
+# Piling Up! 
+# №1
+for _ in range(int(input())):
+    
+    input()
+    side = list(map(int, input().split()))
+    len_of_side = len(side)
+    i = 0
+    
+    while (i < len_of_side - 1) and (side[i] >= side[i+1]):
+        i += 1
+    
+    while (i < len_of_side - 1) and (side[i] <= side[i+1]):
+        i += 1
+    
+    if i == len_of_side - 1:    
+        print("Yes")  
+    else: 
+        print("No")
+
+
+# №2 using deque
+from collections import deque
+
+for _ in (range(int(input()))):
+    input()
+    side_lengths = deque(map(int, input().strip().split()))
+    result = "Yes"
+    
+    if max(side_lengths) not in (side_lengths[0], side_lengths[-1]):
+        result = "No"
+    print(result)
+
+
 # Most Common
 # You are given a string S. 
 # The string contains only lowercase English alphabet characters.
@@ -116,3 +183,6 @@ class OrderedCounter(Counter, OrderedDict):
     pass
 
 [print(*c) for c in OrderedCounter(sorted(input())).most_common(3)]
+
+
+
