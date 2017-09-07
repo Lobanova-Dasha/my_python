0da5103ff0f4c82e2bc6ebac8502eb56a8cd2dac
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-09-07 16:03:54 +0300

    Updated

--- hackerrank/regexp.py
+++ hackerrank/regexp.py
@@ -51,3 +51,134 @@ for _ in range(int(input())):
         line = line.replace(" && ", " and ").replace(" || ", " or ")
     
     print(line)
+
+
+#7 Validating Roman Numerals
+thousand = 'M{0,3}'
+hundred = '(C[MD]|D?C{0,3})'
+ten = '(X[CL]|L?X{0,3})'
+digit = '(I[VX]|V?I{0,3})'
+
+print(bool(re.match(thousand + hundred+ten+digit +'$', input())))
+
+# I'm shocked. Python has Roman module
+from roman import fromRoman
+
+try:
+    if 0<fromRoman(input())<4000:
+        print(True)
+    else:
+  
+        print(False)
+except:
+    print(False)
+
+
+#8 Validating phone numbers
+for i in range(int(input())):
+    if re.match(r'[789]\d{9}$', input()):   
+        print('YES')  
+    else:  
+        print('NO')  
+
+
+#9 Hex Color Code
+for i in range(int(input())):
+    matches = re.findall(r':?.(#[0-9a-fA-F]{6}|#[0-9a-fA-F]{3})', input())
+    
+    if matches:
+        print(*matches, sep='\n')        
+
+
+#10 Validating and Parsing Email Addresses
+import email.utils
+
+for _ in range(int(input())):
+    t = email.utils.parseaddr(input())
+
+    if bool(re.match('[a-zA-Z](\w|-|\.)*@[a-zA-Z]*\.[a-zA-Z]{0,3}$',t[1])):
+        print(email.utils.formataddr(t))
+
+
+#11 HTML Parser - Part 1
+from html.parser import HTMLParser
+
+class MyHTMLParser(HTMLParser):
+
+    def handle_starttag(self, tag, attrs):        
+        print('Start :',tag)
+        for elem in attrs:
+            print('->',elem[0],'>',elem[1])
+            
+    def handle_endtag(self, tag):
+        print('End   :',tag)
+        
+    def handle_startendtag(self, tag, attrs):
+        print('Empty :',tag)
+        for elem in attrs:
+            print('->',elem[0],'>',elem[1])
+            
+MyParser = MyHTMLParser()
+MyParser.feed(''.join([input().strip() for _ in range(int(input()))]))
+
+
+#12
+#from html.parser import HTMLParser
+class CustomHTMLParser(HTMLParser):
+    
+    def handle_comment(self, data):
+        number_of_line = len(data.split('\n'))
+        if number_of_line>1:
+            print('>>> Multi-line Comment')
+        else:
+            print('>>> Single-line Comment')
+        if data.strip():
+            print(data)
+
+    def handle_data(self, data):
+        if data.strip():
+            print(">>> Data")
+            print(data)
+
+parser = CustomHTMLParser()
+
+n = int(input())
+
+html_string = ''
+for i in range(n):
+    html_string += input().rstrip()+'\n'
+    
+parser.feed(html_string)
+parser.close()
+
+
+#13 Detect HTML Tags, Attributes and Attribute Values
+#from html.parser import HTMLParser
+class MyHTMLParser(HTMLParser):
+    
+    def handle_starttag(self, tag, attrs):
+        print(tag)
+        [print('-> {} > {}'.format(*attr)) for attr in attrs]
+        
+html = '\n'.join([input() for _ in range(int(input()))])
+parser = MyHTMLParser()
+parser.feed(html)
+parser.close()
+
+
+#14 Validating UID
+
+for _ in range(int(input())):
+    
+    uid = ''.join(sorted(input()))
+    
+    try:
+        assert re.search(r'[A-Z]{2}', uid)
+        assert re.search(r'\d\d\d', uid)
+        assert not re.search(r'[^a-zA-Z0-9]', uid)
+        assert not re.search(r'(.)\1', uid)
+        assert len(uid) == 10
+    except:
+        print('Invalid')
+    else:
+        print('Valid')
