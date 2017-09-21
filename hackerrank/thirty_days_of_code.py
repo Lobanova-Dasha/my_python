ed2900288659111136481a5f3313d09e011bdd91
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-09-21 23:57:07 +0300

    Updated

--- hackerrank/thirty_days_of_code.py
+++ hackerrank/thirty_days_of_code.py
@@ -238,3 +238,18 @@ for i in range(0, p):
     print(s)
     
   
+# Day 26: Nested Logic
+import sys
+
+inlist = sys.stdin.readlines()
+ad, am, ay = map(int, inlist[0].strip().split())
+ed, em, ey = map(int, inlist[1].strip().split())
+
+if ay > ey: 
+    print(10000)
+elif ay == ey and am > em: 
+    print((am-em)*500)
+elif ay == ey and am == em and ad > ed: 
+    print((ad-ed)*15)
+else: 
+    print(0)
