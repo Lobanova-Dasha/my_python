4f2f1aa11306ed50947fbc856a0066a0d9052a35
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-09-22 03:26:26 +0300

    Updated

--- hackerrank/thirty_days_of_code.py
+++ hackerrank/thirty_days_of_code.py
@@ -218,7 +218,38 @@ class Student(Person):
 # scores = list( map(int, input().split()) )
 # s = Student(firstName, lastName, idNum, scores)
 # s.printPerson()
-# print("Grade:", s.calculate())            
+# print("Grade:", s.calculate())
+
+
+# Day 13: Abstract Classes 
+from abc import ABCMeta, abstractmethod
+
+class Book(metaclass=ABCMeta):
+    def __init__(self,title,author):
+        self.title = title
+        self.author = author   
+    
+    @abstractmethod
+    def display(): 
+        pass
+
+
+class MyBook(Book):
+    def __init__(self,title,author, price):
+        super().__init__(title, author)
+        self.price = price
+
+    def display(self):
+        print("Title: {}".format(self.title))
+        print("Author: {}".format(self.author))
+        print("Price: {}".format(self.price))            
+
+
+# title = input()
+# author = input()
+# price = int(input())
+# new_novel = MyBook(title,author, price)
+# new_novel.display()                             
 
 
 #Day 14: Scope 
