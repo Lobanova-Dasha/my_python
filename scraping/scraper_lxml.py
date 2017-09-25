81ec9e4fcfb6df4740c6760054d91fb21e2e76d7
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-09-25 18:06:28 +0300

    Updated scraper.lxml.py

--- scraping/scraper_lxml.py
+++ scraping/scraper_lxml.py
@@ -3,26 +3,25 @@
 
 import sys
 from lxml import html
-from lxml import etree
 import requests
 import re
 
 valid_date = r"(0[1-9]|[12][0-9]|3[01])[- /.](0[1-9]|1[012])[- /.](19|20)\d\d"
 
     
-def get_url(**kwargs):
 
-    '''Validate dates if True, else exit'''
+def get_article_info(**kwargs):
+
+    my_url = "https://www.gazeta.ru/search.shtml?p=search&page=0&text={text}&article=&section=&from={from}&to={to}&sort_order=published_desc&input=utf8"
+    
     
     if re.match(valid_date, params["from"]) and re.match(valid_date, params["to"]):
-
-        my_url = "https://www.gazeta.ru/search.shtml?p=search&page=0&text={text}&article=&section=&from={from}&to={to}&sort_order=published_desc&input=utf8".format(**params)
-        return my_url
-
+        return requests.get(my_url, params=params) 
+        
     else:
         print("Sorry, you have typed a wrong date")
-        sys.exit()
-      
+        sys.exit()      
+
 
 def parse_articles(requests):
     
@@ -40,22 +39,23 @@ def parse_articles(requests):
     
         num += 1
         
-        '''Date'''
+        """Date"""
         dates = item_lxml.xpath('.//time[@class = "date_time"]/text()')[0]
     
-        '''Author'''
+        """Author"""
         try:
-            author = item_lxml.xpath('.//*[@class = "s_author_name"]/text()')[0]
+            #author = item_lxml.xpath('.//*[@class = "s_author_name"]/text()')[0]
+            author = item_lxml.xpath('.//*[contains(@class, "author_name")]/text()')[0]
         except IndexError as e:
             author = "Неизвестен" 
         
-        '''Title'''
+        """Title"""
         title= item_lxml.xpath('.//h2[@class = "h3 no_float"]/a/text()')[0]       
         
-        '''Link'''
+        """Link"""
         link = item_lxml.xpath('.//h2[@class = "h3 no_float"]/a/@href')[0]
         
-        '''Summary'''
+        """Summary"""
         summary = item_lxml.xpath('.//p[@class = "intro"]/a/text()')[0]
 
   
@@ -71,16 +71,17 @@ def parse_articles(requests):
 
 # the program's execution
 while True:
+
     params = {"text": input("Введите ключевое слово: "),
               "from": input("В период с дд.мм.гггг: "),
                 "to": input("По дд.мм.гггг: ")}
   
-    my_url = get_url(**params)
-    my_req = requests.get(my_url)
+
+    my_req = get_article_info() 
     parse_articles(my_req)
     
-    # Ask an user does he want to continue a searching?
-    value = input("Вы хотите продолжить посик? [y to continue]: ")
+    # Ask an user does he want to continue the search?
+    value = input("Вы хотите продолжить поиск? [y to continue]: ")
         
     if value == 'y':
         continue
