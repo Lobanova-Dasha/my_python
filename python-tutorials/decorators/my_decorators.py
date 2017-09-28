ed5613ab01cefb93e29381497e0b34f122a5b5bf
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-09-29 00:34:58 +0300

    Updated

--- python-tutorials/decorators/my_decorators.py
+++ python-tutorials/decorators/my_decorators.py
@@ -103,3 +103,24 @@ def display_info(name, age):
 # print(display_info.__name__)
 
 display_info('Tom', 22)  
+
+
+# Decorators With Arguments
+def prefix_decorator(prefix):
+    def decorator_func(original_func):
+        def  wrapper_func(*args, **kwargs):
+            print(prefix, 'Еxecuted Before ', original_func.__name__)
+            result = original_func(*args, **kwargs)
+            print(prefix, 'Еxecuted After', original_func.__name__, "\n")
+            return result
+        return wrapper_func
+    return decorator_func    
+
+
+@prefix_decorator('TESTING:')
+def display_info(name, age):
+    print('display_info ran with arguments ({}, {})'.format(name, age)) 
+
+
+#display_info('Travis', 30)  
+
