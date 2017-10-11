65e07dae999b49befb30536a19921518ddc86b2a
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-10-11 16:04:18 +0300

    Updated

--- scraping/flight_scraper.py
+++ scraping/flight_scraper.py
@@ -46,16 +46,42 @@ page = session.post(response.url,
                             }
                     )
 
-print(page.status_code)
-#print(page.text)
+two_ways_page = session.post(response.url, data={
+                      '_ajax[templates][]':'main',
+                      '_ajax[templates][]': 'priceoverview',
+                      '_ajax[templates][]': 'infos',
+                      '_ajax[templates][]': 'flightinfo',
+         '_ajax[requestParams][departure]':	'Berlin - Tegel',
+       '_ajax[requestParams][destination]': 'Munich',
+   '_ajax[requestParams][returnDeparture]': '',
+ '_ajax[requestParams][returnDestination]':'',	
+      '_ajax[requestParams][outboundDate]': '2017-10-27',
+        '_ajax[requestParams][returnDate]':	'2017-10-27',
+        '_ajax[requestParams][adultCount]':	'1',
+        '_ajax[requestParams][childCount]':	'0',
+       '_ajax[requestParams][infantCount]':	'0',
+  '_ajax[requestParams][openDateOverview]':'',	
+            '_ajax[requestParams][oneway]': ''
+                                                },
+                                           headers={
+                          'User-Agent': 'Mozilla/5.0 (Windows NT 6.3; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.48 Safari/537.36',
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                             'Referer': response.url
+                            }
+                                                )	
+
+print(two_ways_page.status_code)
 
 
-tree = html.fromstring(page.json()['templates']['main'], "html.parser")
+tree = html.fromstring(two_ways_page.json()['templates']['main'], "html.parser")
 
+#outbound_block = tree.xpath('//div[@class="outbound block"]')
 box = tree.xpath('//div[@class="current"]')
-for i in box:
-    item = i.xpath('./span/@title')
-    price = float(((((item[0]).split(","))[-1]).split())[-1])
-    print(item[0])
+
+
+for title in box:
+    data = title.xpath('./span/@title')
+    price = float(((((data[0]).split(","))[-1]).split())[-1])
+    print(data[0])
     print(price)
 
