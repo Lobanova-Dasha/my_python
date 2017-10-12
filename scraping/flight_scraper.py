fe5b1b9b569554cf76f4a3b479ccd5a85e014874
Lobanova-Dasha <dary95lobanova@gmail.com>
2017-10-12 16:43:46 +0300

    Updated

--- scraping/flight_scraper.py
+++ scraping/flight_scraper.py
@@ -48,44 +48,66 @@ page = session.post(response.url,
                             }
                     )
 
-
-print('\nStatus code:',page.status_code)
-#print(two_ways_page.json()['templates']['main'])
-# tree = html.fromstring(page.json()['templates']['main'], "html.parser")
-table_head = "    FLIGHT    START/END   DURATION      CLASS          PRICE     "
-
 tree = html.fromstring(page.json()['templates']['main'], "html.parser")
 currency = tree.xpath('//th[@id="flight-table-header-price-ECO_PREM"]/text()')[0]
-#print(currency)
 
-outbound_tree = tree.xpath('//div[@class="outbound block"]//div[@class="lowest"]')
-outbound_flights = [title.xpath('./span/@title')[0] for title in outbound_tree]
-
-return_tree = tree.xpath('//div[@class="return block"]//div[@class="lowest"]')
-return_flights = [title.xpath('./span/@title')[0] for title in return_tree]
+print('\nStatus code:',page.status_code)
 
 
-combinations = [*product(outbound_flights, return_flights)]
-print('The total count of outbound flights: {}'.format(len(outbound_flights)))
-print('The total count of return flights: {}'.format(len(return_flights)))
-print('The total count of flights combinations: {}\n'.format(len(combinations)))
+def oneway_flight(response, coin):
+    #tree = html.fromstring(page.json()['templates']['main'], "html.parser")
 
-result = []
-for i in combinations[:10]:
-    out_price = float(i[0].split()[-1])
-    return_price = float(i[-1].split()[-1])
-    #print("Out {}, return {}".format(out_price, return_price))
-    total = out_price + return_price
-    #print("total price:", round(total,2), currency)
-    element = [i[0], currency, '   ', i[1], currency, '   Total price: ', round(total,2)]
-    result.append(element)
+    box = tree.xpath('//div[@class="lowest"]')
+    flights = [title.xpath('./span/@title') for title in box]
+
+
+    #currency = tree.xpath('//th[@id="flight-table-header-price-ECO_PREM"]/text()')[0]
+    table_head = " FLIGHT      START/END     DURATION      CLASS        PRICE"
     
-i=0
-print(table_head*2)
-for flight in sorted(result, key=itemgetter(-1)):
-    i +=1
-    print(str(i)+')', *flight, currency)
+    print(table_head)
+    i = 0
+    for flight in flights[:10]:
+        i += 1
+        print(str(i)+')', *flight, currency)
+        
+        
 
+def twoways_flight(response, coin):
+    #tree = html.fromstring(page.json()['templates']['main'], "html.parser")
+    #currency = tree.xpath('//th[@id="flight-table-header-price-ECO_PREM"]/text()')[0]
+    #print(currency)
+
+    outbound_tree = tree.xpath('//div[@class="outbound block"]//div[@class="lowest"]')
+    outbound_flights = [title.xpath('./span/@title')[0] for title in outbound_tree]
+
+    return_tree = tree.xpath('//div[@class="return block"]//div[@class="lowest"]')
+    return_flights = [title.xpath('./span/@title')[0] for title in return_tree]
+
+    combinations = [*product(outbound_flights, return_flights)]
+    table_head = "    FLIGHT      START/END     DURATION     CLASS      PRICE     "
+
+    print('The total count of outbound flights: {}'.format(len(outbound_flights)))
+    print('The total count of return flights: {}'.format(len(return_flights)))
+    print('The total count of flights combinations: {}\n'.format(len(combinations)))
+
+    result = []
+    for i in combinations[:10]:
+        out_price = float(i[0].split()[-1])
+        return_price = float(i[-1].split()[-1])
+        #print("Out {}, return {}".format(out_price, return_price))
+        total = out_price + return_price
+        #print("total price:", round(total,2), currency)
+        element = [i[0], currency, '   ', i[1], currency, '   Total price: ', round(total,2)]
+        result.append(element)
+    
+    i=0
+    print(table_head*2)
+    for flight in sorted(result, key=itemgetter(-1)):
+        i +=1
+        print(str(i)+')', *flight, currency)
+
+twoways_flight(tree, currency)
+#oneway_flight(tree, currency)
   
 
 
